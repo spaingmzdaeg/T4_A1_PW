dc52545c8918a087c36c54c3083725eaf09bb8ff
spainchesterdaeg <spainchester@outlook.com>
2020-11-04 18:59:54 -0600

    metodo para obtener promedio general listo

--- Operations.php
+++ Operations.php
@@ -1,9 +1,6 @@
 <?php 
 
-function sayHi($name,$age)
-       {
-           echo "Hellow $name,you are $age <br>";
-       }
+
 function printArray($arrayStudents)
 {
     foreach($arrayStudents as $key => $value) {
@@ -13,6 +10,47 @@ function printArray($arrayStudents)
         }
         echo '<br />';
     }
-}       
+}
+
+function getPersonalAverage($arrayStudents){
+
+
+    foreach($arrayStudents as $key => $value) {
+        echo 'Student: '.$key.'<br />';
+        //echo '<br />';
+        echo 'Student: '.$key.' Average:'.number_format((array_sum($value)/6));
+    
+        foreach($value as $s_key => $s_value) {
+            echo '<br />';
+            echo 'School Subject'.($s_key+1).' => '.$s_value.'<br />';
+        }
+        echo '<br />';
+    }
+
+    
+}
+
+
+function getGroupAverage($arrayStudents){
+
+
+    foreach($arrayStudents as $key => $value) {
+        echo 'Student: '.$key.'<br />';
+        //echo '<br />';
+        echo 'Student: '.$key.' Average:'.number_format((array_sum($value)/6));
+    
+        foreach($value as $s_key => $s_value) {
+            echo '<br />';
+            echo 'School Subject'.($s_key+1).' => '.$s_value.'<br />';
+        }
+        echo '<br />';
+    }
+
+    
+}
+
+
+
+
 
 ?>
